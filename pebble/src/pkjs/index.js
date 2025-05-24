d093dc873dcfc508ffd8a8ffefdadff9dbd5735d
blockarchitech <hello@blockarchitech.com>
2025-05-24 14:54:27 -0700

    feat: pebble app

---
+++ pebble/src/pkjs/index.js
@@ -0,0 +1,14 @@
+Pebble.addEventListener("showConfiguration", function() {
+    Pebble.getTimelineToken(function(token) {
+        var url = 'https://timetodo-282379823777.us-central1.run.app/config/pebble'
+        var query = new URLSearchParams({
+            'timeline_token': token,
+            'account_token': Pebble.getAccountToken(),
+        });
+        url += '?' + query.toString();
+        Pebble.openURL(url);
+    }, function(error) {
+        console.error('Error getting timeline token: ' + error);
+        Pebble.openURL('https://timetodo-282379823777.us-central1.run.app/config/pebble');
+    })
+});
