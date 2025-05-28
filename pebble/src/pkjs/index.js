b1d5cf32508d7d63ec8150cfce7755d3c555a004
blockarchitech <hello@blockarchitech.com>
2025-05-27 20:56:34 -0700

    chore: license headers for pebble stuff

--- pebble/src/pkjs/index.js
+++ pebble/src/pkjs/index.js
@@ -1,13 +1,25 @@
+/*
+ *    Copyright 2025 blockarchitech
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 var loggedInConfig = require('./config-loggedin.json');
 var loggedOutConfig = require('./config-loggedout.json');
 var customClay = require('./custom-clay.js');
 
 var Clay = require('pebble-clay');
-// var clay = new Clay(loggedOutConfig, customClay, { autoHandleEvents: false, userData: {
-//     authorization: auth_header,
-//     timeline_token: timeline_token,
-//     account_token: account_token
-// } });
+
 var clay = new Clay(loggedOutConfig, customClay, { autoHandleEvents: false, userData: {
     abc: '123',
 } });
